def3e08c39d843dcdb95ddba3ea80a1891015cca
JuanPedrazaD <juan_pedrazaun.edu.co>
2025-08-21 11:04:47 -0500

    fix(auth): Se arregla la inserción y busqueda del usuario en base de datos

--- src/domain/auth/entities/user-auth.pstgs.entity.ts
+++ src/domain/auth/entities/user-auth.pstgs.entity.ts
@@ -1,13 +1,13 @@
 import { Column, Entity, PrimaryColumn } from 'typeorm';
 
-@Entity({ name: 'USUARIOS' })
+@Entity({ name: 'user' })
 export class UserAuthEntity {
-  @PrimaryColumn({ name: 'ID', type: 'int' }) // Se maneja con una secuencia en Oracle
+  @PrimaryColumn({ name: 'id', type: 'int' }) // Se maneja con una secuencia en Oracle
   id: number;
 
-  @Column({ name: 'USERNAME', length: 150, unique: true })
+  @Column({ name: 'username', length: 150, unique: true })
   username: string;
 
-  @Column({ name: 'PASSWORD', length: 255 })
+  @Column({ name: 'password', length: 255 })
   password: string;
 }
