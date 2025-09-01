dd20ec0f1e52ac487118fef437fbd10354742539
JuanPedrazaD <juan_pedrazaun.edu.co>
2025-09-01 16:47:27 -0500

    feat(group-notification): Adelanto de servicio para envio de notificaciones por grupos

--- src/application/notification/send-options/push-notification/group-notification.service.ts
+++ src/application/notification/send-options/push-notification/group-notification.service.ts
@@ -1,8 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+
+import { Repository } from 'typeorm';
+
+import { NotificationGroupDto } from 'src/domain/notification/dto/send/notification-group-request.dto';
+import { SendHistoryEntity } from 'src/domain/notification/entity/send-history.pstgs.entity';
+import { SaveNotificationService } from '../../save-notification.service';
+
+const DEFAULT_NOTIFICATION_ID = 1; // Valor por defecto si no se guarda la notificación
 
 @Injectable()
 export class GroupNotificationService {
-  constructor() {}
+  constructor(
+    @Inject('SEND_HISTORY_REPOSITORY')
+    private readonly sendHistoryRepository: Repository<SendHistoryEntity>,
+    private readonly saveNotificationService: SaveNotificationService,
+  ) {}
 
-  async sendNotification() {}
+  async sendNotification(notificationGroupDto: NotificationGroupDto) {
+    const { notificationType, saveNotification, title, message, appId } =
+      notificationGroupDto;
+
+    // Cuando se decide no guardar la notificación se utiliza el id 1
+    let notificationId: number = DEFAULT_NOTIFICATION_ID;
+
+    let saved: boolean = true;
+
+    //Si el parametro es true se guarda la notificación o si existe se utiliza el id
+    if (saveNotification == true) {
+      const response = await this.saveNotificationService.saveNotification(
+        title,
+        message,
+        notificationType,
+        appId,
+      );
+      notificationId = response.id;
+      saved = response.saved;
+    }
+
+    //? Anotaciones para guardar el titulo y el mensaje cuando el parametro save es false
+    let anotations: string = '';
+    if (saveNotification == false) {
+      anotations = `title: ${title}, message: ${message}`;
+    }
+  }
 }
